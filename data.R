e1b4fb7c80fd9809492a64a7eafb61d846fa9b95
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-10-05 11:15:08 +0500

    Update data.R
    Added option to get data for multiple bonds

--- data.R
+++ data.R
@@ -1,28 +1,33 @@
 library(rvest)
 
-smartlab.gov.bond <- function(x){
+smartlab.gov.bond <- function(x){ # Get data for Russian government bonds
   
-  l <- read_html(sprintf("https://smart-lab.ru/q/bonds/%s/", x)) %>%
-    html_nodes("article") %>% html_nodes("div") %>% html_nodes("div") %>%
-    html_text()
+  D <- NULL
   
-  l <- gsub('["\n"]', '', gsub('["\t"]', '',l))
-  
-  L <- cbind.data.frame(
-    l[seq(from = 2, to = length(l), by = 3)],
-    l[seq(from = 3, to = length(l), by = 3)]
-  )
-  
-  L <- L[c(-18, -27, -31),]
-  
-  v <- L[,1]
+  for (n in 1:length(x)){
     
-  L <- as.data.frame(L[,-1])
+    l <- read_html(sprintf("https://smart-lab.ru/q/bonds/%s/", x[n])) %>%
+      html_nodes("article") %>% html_nodes("div") %>% html_nodes("div") %>%
+      html_text()
+    
+    l <- gsub('["\n"]', '', gsub('["\t"]', '',l)) # Clean and assign data
+    
+    L <- cbind.data.frame(
+      l[seq(from = 2, to = length(l), by = 3)],
+      l[seq(from = 3, to = length(l), by = 3)]
+    )
+    
+    L <- L[c(-18, -27, -31),] # Reduce empty rows
+    
+    v <- L[,1] # Save values for row names
+    
+    L <- as.data.frame(L[,-1])
+    
+    rownames(L) <- v # Assign row names
+    colnames(L) <- x[n]
+    
+    if (is.null(D)) D <- L else D <- cbind(D, L) } # Merge
   
-  rownames(L) <- v
-  
-  colnames(L) <- x
-  
-  L
+  D # Output
 }
-smartlab.gov.bond("SU26229RMFS3")
+smartlab.gov.bond(c("SU26229RMFS3", "SU26219RMFS4")) # Test
